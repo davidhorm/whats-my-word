6c662cbe91ed773d316c126afc4c70db93531136
davidhorm <davidhorm+github@gmail.com>
2020-04-28 21:39:16 -0700

    Fix linting on default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
+/**
+ * App component.
+ *
+ * @returns {object} - <App />
+ */
 function App() {
   return (
     <div className="App">
@@ -10,12 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
