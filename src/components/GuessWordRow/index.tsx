4ba517a6f9f577c76864dbe86e85b49cc7c0542f
davidhorm <davidhorm+github@gmail.com>
2020-05-02 15:23:37 -0700

    Implement <GuessWordRow />

--- src/components/GuessWordRow/index.tsx
+++ src/components/GuessWordRow/index.tsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { getLetterGridAreaName } from '../../services/grid-template.service';
 
 const propTypes = {
-  /** The Start Index of the Guess Word. */
-  startIndex: PropTypes.number,
+  /** The row index of the Guess Word. */
+  rowIndex: PropTypes.number.isRequired,
 
   /** The Guess Word. */
   guessWord: PropTypes.string,
@@ -12,15 +13,19 @@ const propTypes = {
 type props = PropTypes.InferProps<typeof propTypes>;
 
 /**
- * <GuessWordRow /> displays the letters of the guess word in a grid.
+ * <GuessWordRow /> displays the letters of the guess word in individual <span> tags with grid-area styles.
  *
  * @returns {object} - a bunch of <span> tags
  */
-const GuessWordRow: React.FC<props> = ({ startIndex, guessWord }) => {
+const GuessWordRow: React.FC<props> = ({ rowIndex, guessWord }) => {
+  const letters = guessWord?.split('');
   return (
-    <span>
-      startIndex: {startIndex}; guessWord: {guessWord}
-    </span>
+    <>
+      {letters &&
+        letters.map((letter, letterIndex) => (
+          <span style={{ gridArea: getLetterGridAreaName(rowIndex, letterIndex) }}>{letter}</span>
+        ))}
+    </>
   );
 };
 
