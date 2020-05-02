afbcd979ea46848c7a2dae762f8ce994a115d541
davidhorm <davidhorm+github@gmail.com>
2020-05-02 15:33:54 -0700

    Add sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { YourWord } from './components/YourWord';
 
 /**
  * App component.
@@ -9,17 +8,9 @@ import logo from './logo.svg';
  */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <YourWord actualWordLength={6} />
+    </main>
   );
 }
 
--- src/components/YourWord/index.tsx
+++ src/components/YourWord/index.tsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { getGridTemplateAreas } from '../../services/grid-template.service';
+import { GuessWordRow } from '../GuessWordRow';
 
 const propTypes = {
   /** The max length of the Actual Word. */
-  actualWordLength: PropTypes.number,
+  actualWordLength: PropTypes.number.isRequired,
 };
 
 type props = PropTypes.InferProps<typeof propTypes>;
@@ -14,7 +16,22 @@ type props = PropTypes.InferProps<typeof propTypes>;
  * @returns {object} - I don't know yet.
  */
 const YourWord: React.FC<props> = ({ actualWordLength }) => {
-  return <span>actualWordLength: {actualWordLength}</span>;
+  const gridStyle = {
+    display: 'grid',
+    columnGap: '1rem',
+    gridTemplateColumns: '1rem',
+    gridTemplateAreas: getGridTemplateAreas(actualWordLength),
+  };
+
+  const sampleGuessWords = ['by', 'now', 'are', 'sin', 'pus', 'hose', 'hair', 'chop', 'chops', 'house', 'choose'];
+
+  return (
+    <section style={gridStyle}>
+      {sampleGuessWords.map((guessWord, rowIndex) => (
+        <GuessWordRow rowIndex={rowIndex} guessWord={guessWord} />
+      ))}
+    </section>
+  );
 };
 
 YourWord.propTypes = propTypes;
