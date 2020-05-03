31fb720e4b89cd95b2a9c00598fa9554ed2f2f8c
davidhorm <davidhorm+github@gmail.com>
2020-05-03 15:25:46 -0700

    Implemented getGuessWordScore

--- src/components/YourWord/services/guess-word-scorer.service.ts
+++ src/components/YourWord/services/guess-word-scorer.service.ts
@@ -6,7 +6,38 @@
  * @param {string} actualWord - Actual Word
  * @returns {number} - Score of the Guess Word.
  */
-const getGuessWordScore = (startIndex: number, guessWord: string, actualWord: string): number =>
-  startIndex + guessWord.length + actualWord.length;
+const getGuessWordScore = (startIndex: number, guessWord: string, actualWord: string): number => {
+  const actualLetters = actualWord.split('');
+
+  const scores = guessWord
+    .split('')
+    .map((guessLetter, guessLetterIndex): string | number => {
+      // if exact match, then replace letter with 1000
+      // and blank corresponding letter in `actualLetters`
+      const actualLetterIndex = guessLetterIndex + startIndex;
+      if (guessLetter === actualLetters[actualLetterIndex]) {
+        actualLetters[actualLetterIndex] = '';
+        return 1000;
+      }
+
+      return guessLetter;
+    })
+    .map((remainingGuessLetter): number => {
+      // if remaining match, then replace letter with 250
+      // and blank corresponding letter in `actualLetters`
+      // else replace letter with 0
+      if (typeof remainingGuessLetter === 'string') {
+        const actualLetterIndex = actualLetters.indexOf(remainingGuessLetter);
+        if (actualLetterIndex >= 0) {
+          actualLetters[actualLetterIndex] = '';
+          return 250;
+        }
+        return 0;
+      }
+      return remainingGuessLetter;
+    });
+
+  return scores.reduce((sum, currentValue) => sum + currentValue);
+};
 
 export { getGuessWordScore };
