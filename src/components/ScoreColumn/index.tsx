fad636ef648077c301c603cbc1e6bb5a5317d8cb
davidhorm <davidhorm+github@gmail.com>
2020-05-03 21:26:21 -0700

    Implemented <ScoreColumn />

--- src/components/ScoreColumn/index.tsx
+++ src/components/ScoreColumn/index.tsx
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { getScoreGridAreaName, GRID_AREA_NAMES } from '../../services/grid-template.service';
 
 const propTypes = {
   /** The score for each guess word. */
-  scores: PropTypes.arrayOf(PropTypes.number),
+  scores: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+
+  /** The max length of the Actual Word. */
+  actualWordLength: PropTypes.number.isRequired,
 };
 
 type props = PropTypes.InferProps<typeof propTypes>;
@@ -13,8 +17,19 @@ type props = PropTypes.InferProps<typeof propTypes>;
  *
  * @returns {object} - a bunch of <span> tags
  */
-const ScoreColumn: React.FC<props> = ({ scores }) => {
-  return <span>scores: {scores}</span>;
+const ScoreColumn: React.FC<props> = ({ scores, actualWordLength }) => {
+  const bonusScore = scores[scores.length - 1] === actualWordLength * 1000 ? 3000 : 0;
+  const finalScore = scores.reduce((sum, currentValue) => sum + currentValue, 0);
+
+  return (
+    <>
+      {scores?.map((score, rowIndex) => (
+        <span style={{ gridArea: getScoreGridAreaName(rowIndex) }}>{score}</span>
+      ))}
+      <span style={{ gridArea: GRID_AREA_NAMES.BONUS_SCORE_VALUE }}>{bonusScore}</span>
+      <span style={{ gridArea: GRID_AREA_NAMES.FINAL_SCORE_VALUE }}>{finalScore}</span>
+    </>
+  );
 };
 
 ScoreColumn.propTypes = propTypes;
--- src/components/YourWord/index.tsx
+++ src/components/YourWord/index.tsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { getGridTemplateAreas } from '../../services/grid-template.service';
+import { getGridTemplateAreas, GRID_AREA_NAMES } from '../../services/grid-template.service';
 import { GameWord } from '../GameWord';
 import { GuessWordRow } from '../GuessWordRow';
+import { ScoreColumn } from '../ScoreColumn';
+import { getGuessWordScore } from './services/guess-word-scorer.service';
 
 const propTypes = {
   /** The max length of the Actual Word. */
@@ -24,14 +26,21 @@ const YourWord: React.FC<props> = ({ actualWordLength }) => {
     gridTemplateAreas: getGridTemplateAreas(actualWordLength),
   };
 
+  const sampleActualWord = 'choose';
   const sampleGuessWords = ['by', 'now', 'are', 'sin', 'pus', 'hose', 'hair', 'chop', 'chops', 'house', 'choose'];
+  const scores = sampleGuessWords.map((guessWord, rowIndex) =>
+    getGuessWordScore(rowIndex, guessWord, sampleActualWord)
+  );
 
   return (
     <section style={gridStyle}>
-      <GameWord actualWord="choose" />
+      <GameWord actualWord={sampleActualWord} />
       {sampleGuessWords.map((guessWord, rowIndex) => (
         <GuessWordRow rowIndex={rowIndex} guessWord={guessWord} />
       ))}
+      <ScoreColumn actualWordLength={actualWordLength} scores={scores} />
+      <span style={{ gridArea: GRID_AREA_NAMES.BONUS_SCORE_LABEL }}>Bonus Points:</span>
+      <span style={{ gridArea: GRID_AREA_NAMES.FINAL_SCORE_LABEL }}>Final Score:</span>
     </section>
   );
 };
