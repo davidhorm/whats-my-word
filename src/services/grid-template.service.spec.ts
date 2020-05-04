068bab7dc0add17f92294faf20daf7ec12556335
davidhorm <davidhorm+github@gmail.com>
2020-05-03 17:22:50 -0700

    Refactored grid template to include actual word, and final scoring rows

--- src/services/grid-template.service.spec.ts
+++ src/services/grid-template.service.spec.ts
@@ -7,6 +7,7 @@ describe('grid-template.service', () => {
         const actual = getGridTemplateAreas(6);
 
         const expectedRows = [
+          '". actual-letter-0 actual-letter-1 actual-letter-2 actual-letter-3 actual-letter-4 actual-letter-5 . ."',
           '". row-0-letter-0 row-0-letter-1 . . . . row-0-score ."',
           '". row-1-letter-0 row-1-letter-1 row-1-letter-2 . . . row-1-score ."',
           '". . row-2-letter-0 row-2-letter-1 row-2-letter-2 . . row-2-score ."',
@@ -18,6 +19,8 @@ describe('grid-template.service', () => {
           '". row-8-letter-0 row-8-letter-1 row-8-letter-2 row-8-letter-3 row-8-letter-4 . row-8-score ."',
           '". . row-9-letter-0 row-9-letter-1 row-9-letter-2 row-9-letter-3 row-9-letter-4 row-9-score ."',
           '". row-10-letter-0 row-10-letter-1 row-10-letter-2 row-10-letter-3 row-10-letter-4 row-10-letter-5 row-10-score ."',
+          '". bonus-score-label bonus-score-label bonus-score-label bonus-score-label bonus-score-label bonus-score-label bonus-score ."',
+          '". final-score-label final-score-label final-score-label final-score-label final-score-label final-score-label final-score ."',
         ];
         const expected = expectedRows.join(' ');
         expect(actual).toBe(expected);
--- src/services/grid-template.service.ts
+++ src/services/grid-template.service.ts
@@ -1,4 +1,74 @@
 /**
+ * Get the grid row template with dots and quotes around the grid column names.
+ *
+ * @param {string} gridColumnNames - Grid Column Names
+ * @returns {string} - Grid Column Names wraped in dots and quotes.
+ */
+const getGridRowTemplate = (gridColumnNames: string) => `". ${gridColumnNames} ."`;
+
+// #region getActualLettersRow
+
+/**
+ * Get the class/grid area name for the Actual Letter span based column index.
+ *
+ * @param {number} letterIndex - Index of the letter position of the Actual Word.
+ * @returns {string} - Text used to set the HTML class/grid area name.
+ */
+export const getActualLetterGridAreaName = (letterIndex: number) => `actual-letter-${letterIndex}`;
+
+/**
+ * Get the Grid Column Names for the letters of the actual word.
+ *
+ * @param {number} actualWordLength - Actual Word length.
+ * @returns {string[]} - Grid Column Names for the letters of the actual word.
+ */
+const getActualLettersRow = (actualWordLength: number) => {
+  const actualLetters = new Array(actualWordLength + 1)
+    .fill('')
+    .map((_, actualLetterIndex) =>
+      actualLetterIndex < actualWordLength ? getActualLetterGridAreaName(actualLetterIndex) : '.'
+    )
+    .join(' ');
+
+  return [getGridRowTemplate(actualLetters)];
+};
+
+// #endregion getActualLettersRow
+
+// #region getFinalScoreRows
+
+export const GRID_AREA_NAMES = {
+  BONUS_SCORE_LABEL: 'bonus-score-label',
+  BONUS_SCORE_VALUE: 'bonus-score',
+  FINAL_SCORE_LABEL: 'final-score-label',
+  FINAL_SCORE_VALUE: 'final-score',
+};
+
+/**
+ * Get the Grid Column Names for the bonus and final scoring rows.
+ *
+ * @param {number} actualWordLength - Actual Word length.
+ * @returns {string[]} - Grid Column Names for the bonus and final scoring rows.
+ */
+const getFinalScoreRows = (actualWordLength: number) => {
+  const bonusScoreRow = new Array(actualWordLength)
+    .fill(GRID_AREA_NAMES.BONUS_SCORE_LABEL)
+    .concat([GRID_AREA_NAMES.BONUS_SCORE_VALUE])
+    .join(' ');
+
+  const finalScoreRow = new Array(actualWordLength)
+    .fill(GRID_AREA_NAMES.FINAL_SCORE_LABEL)
+    .concat([GRID_AREA_NAMES.FINAL_SCORE_VALUE])
+    .join(' ');
+
+  return [getGridRowTemplate(bonusScoreRow), getGridRowTemplate(finalScoreRow)];
+};
+
+// #endregion getFinalScoreRows
+
+// #region getGuessLetterRows
+
+/**
  * Get the class/grid area name for the Letter span based on the row and column index.
  *
  * @param {number} rowIndex - Guess Word row index.
@@ -16,16 +86,16 @@ export const getScoreGridAreaName = (rowIndex: number) => `row-${rowIndex}-score
 export const getLetterGridAreaName = (rowIndex: number, letterIndex: number) => `row-${rowIndex}-letter-${letterIndex}`;
 
 /**
- * Get the CSS Grid template areas value based on the Actual Word length.
+ * Get the Grid Column Names for the letters of the guess word, and score column.
  *
  * @param {number} actualWordLength - Actual Word length.
- * @returns {string} - Text used to see the CSS Grid template areas.
+ * @returns {string[]} - Grid Column Names for the letters of the guess word, and score column.
  */
-export const getGridTemplateAreas = (actualWordLength: number) => {
+const getGuessLetterRows = (actualWordLength: number) => {
   const guessWordStartIndexes = [0, 0, 1, 2, 3, 2, 1, 0, 0, 1, 0];
   const guessWordLengths = [-4, -3, -3, -3, -3, -2, -2, -2, -1, -1, 0];
 
-  const expectedRows = guessWordStartIndexes.map((guessWordStartIndex, rowIndex) => {
+  return guessWordStartIndexes.map((guessWordStartIndex, rowIndex) => {
     const guessWordLength = actualWordLength + guessWordLengths[rowIndex];
 
     const emptySpacesBeforeLetters = guessWordStartIndex + 1;
@@ -39,8 +109,25 @@ export const getGridTemplateAreas = (actualWordLength: number) => {
       .map((_, guessWordLetterIndex) => `${getLetterGridAreaName(rowIndex, guessWordLetterIndex)} `)
       .join('');
 
-    return `". ${dotsBeforeLetters}${letterGridAreaNames}${dotsAfterLetters}${getScoreGridAreaName(rowIndex)} ."`;
+    const gridColumnNames = `${dotsBeforeLetters}${letterGridAreaNames}${dotsAfterLetters}${getScoreGridAreaName(
+      rowIndex
+    )}`;
+    return getGridRowTemplate(gridColumnNames);
   });
+};
 
-  return expectedRows.join(' ');
+// #endregion getGuessLetterRows
+
+/**
+ * Get the CSS Grid template areas value based on the Actual Word length.
+ *
+ * @param {number} actualWordLength - Actual Word length.
+ * @returns {string} - Text used to see the CSS Grid template areas.
+ */
+export const getGridTemplateAreas = (actualWordLength: number) => {
+  const actualLettersRow = getActualLettersRow(actualWordLength);
+  const guessLetterRows = getGuessLetterRows(actualWordLength);
+  const finalScoreRows = getFinalScoreRows(actualWordLength);
+
+  return actualLettersRow.concat(guessLetterRows).concat(finalScoreRows).join(' ');
 };
