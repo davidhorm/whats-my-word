fe3bf04ecaa43cd4e36b9762913e279337184461
David Horm <davidhorm@gmail.com>
2022-01-15 15:48:26 -0800

    Add useInitGameState

---
+++ src/domain/game-word-service/index.ts
@@ -0,0 +1,11 @@
+/** The length of the word to guess */
+export type GameWordLength = 6 | 7;
+
+/** The validated word to guess */
+export type ValidGameWord = string;
+
+/** Code representing the Game Word to guess. The Code is the index of the word in a dictionary array. */
+export type GameWordCode = string;
+
+export { GenerateGameWordCode, GenerateRandomGameWordCode, GetGameWordCodeValidationRule, TransformToGameWord } from './game-word-code-service';
+export { GetGameWordValidationRule } from "./game-word-validation-service";
---
+++ src/use-cases/use-init-game-state.ts
@@ -0,0 +1,3 @@
+import { GenerateGameWordCode, GenerateRandomGameWordCode, GetGameWordCodeValidationRule, GetGameWordValidationRule } from '../domain/game-word-service';
+
+export const useInitGameState = { GetGameWordCodeValidationRule, GetGameWordValidationRule, GenerateGameWordCode, GenerateRandomGameWordCode };
