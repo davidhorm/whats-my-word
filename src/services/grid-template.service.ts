3928d78ed325e4d353492499eacc0a5400eb5c38
David Horm <davidhorm@gmail.com>
2022-02-07 14:19:53 -0800

    Remove unused grid template service

--- src/services/grid-template.service.ts
+++
@@ -1,5 +0,0 @@
-/**
- * The guess word lengths. Add with the actual word length to get the value.
- * The position in the array represents the row index.
- */
-export const GUESS_WORD_LENGTHS = [-4, -3, -3, -3, -3, -2, -2, -2, -1, -1, 0];
