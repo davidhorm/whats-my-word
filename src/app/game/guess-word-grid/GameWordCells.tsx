28b58a97697c5346d7879aae2d2975766432711f
David Horm <davidhorm@gmail.com>
2022-03-06 20:29:55 -0800

    Refactor styles to IndividualCharacterInputs. Refactored disabled color. Add focus color.

--- src/app/game/guess-word-grid/GameWordCells.tsx
+++ src/app/game/guess-word-grid/GameWordCells.tsx
@@ -5,11 +5,15 @@ type GameWordCellsProps = Pick<ClientGameState, 'gameWordRevealed' | 'gameWordLe
 
 export const GameWordCells = ({ gameWordLength, gameWordRevealed = '' }: GameWordCellsProps) => {
   const inputProps = Array.from({ length: gameWordLength }).map((_, index) => ({
-    className: `rounded-sm border border-black/40 p-0 ring-inset uppercase text-2xl text-center ${
-      !!gameWordRevealed && 'bg-zinc-500/25 text-black/40'
-    }`,
     style: { gridArea: `game-word-letter-${index}` },
   }));
 
-  return <IndividualCharacterInputs amount={gameWordLength} value={gameWordRevealed} inputProps={inputProps} />;
+  return (
+    <IndividualCharacterInputs
+      amount={gameWordLength}
+      value={gameWordRevealed}
+      disabled={!!gameWordRevealed}
+      inputProps={inputProps}
+    />
+  );
 };
